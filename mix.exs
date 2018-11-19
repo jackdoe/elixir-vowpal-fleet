3cde239019f36d80da077d6cd6e9093ef5336b02
Borislav Nikolov <borislav.nikolov@booking.com>
2018-11-19 21:29:50 +0100

    bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VowpalFleet.MixProject do
   def project do
     [
       app: :vowpal_fleet,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
